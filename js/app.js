ad8381ccfb05318216020631ba54bfd6d054286e
freda nada <fredasnada@gmail.com>
2017-02-07 13:03:13 -0500

    add listener

--- js/app.js
+++ js/app.js
@@ -4,3 +4,12 @@
 window.addEventListener('keydown', ()=>{
   document.getElementById('header').innerHTML = ''
 })
+// create black box
+const box = document.querySelector('a-box')
+const sky = document.querySelector('a-sky')
+
+box.addEventListener('click', function () {
+  box.setAttribute('color', 'blue')
+  sky.setAttribute('src', '#')
+  sky.setAttribute('color', 'black')
+})
